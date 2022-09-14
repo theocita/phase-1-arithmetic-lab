66deba818617febc1af4e64d46166831aab0f61c
Theocita <theocitaallen@gmail.com>
2022-09-13 21:55:44 -0400

    Progress

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 let num1 = 2;
 let num2 = 31;
 let multiply = num1 * num2;
-console.log(multiply)
+console.log(multiply);
 
-function random(1,10){
-    return Math.floor(Number.isInteger(random) * )
+let random = generateRandomInteger(max) {
+    return Math.floor(Math.random() * max) + 1;
 }
+
+
+let num3 = 6;
+let num4 = 2;
+let mod = num3 - num4;
+console.log(mod);
+
+let max = (2,20);
+Math.max(2,20);
